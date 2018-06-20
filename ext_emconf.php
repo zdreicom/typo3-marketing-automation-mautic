61f4e24fa40875f43cf709fe8acba49a5731a486
Nicole Cordes <nicole@biz-design.biz>
2018-06-20 11:56:35 +0200

    Add basic extension files

---
+++ ext_emconf.php
@@ -0,0 +1,38 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "marketing_automation_mautic".
+ *
+ * Auto generated 20-06-2018 11:55
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'Marketing Automation - Mautic Adapter',
+  'description' => '',
+  'category' => 'fe',
+  'state' => 'stable',
+  'uploadfolder' => 0,
+  'createDirs' => '',
+  'clearCacheOnLoad' => 0,
+  'version' => '0.1.0',
+  'constraints' =>
+  array (
+    'depends' =>
+    array (
+      'typo3' => '8.7.0-8.7.99',
+      'marketing_automation' => '',
+    ),
+    'conflicts' =>
+    array (
+    ),
+    'suggests' =>
+    array (
+    ),
+  ),
+  '_md5_values_when_last_written' => 'a:2:{s:13:"composer.json";s:4:"d49b";s:36:"Resources/Public/Icons/Extension.svg";s:4:"4473";}',
+);
+
