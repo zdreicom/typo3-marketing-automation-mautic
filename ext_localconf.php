3996f79e65679d065c6e241cdf188ff5e36b3ada
Woeler <woeler@esoleaderboards.com>
2018-08-09 15:37:55 +0200

    Add backslashes to full classpaths

--- ext_localconf.php
+++ ext_localconf.php
@@ -22,16 +22,16 @@ call_user_func(function () {
     // Assign the hooks for pushing newly created and edited forms to Mautic
     if (TYPO3_MODE === 'BE') {
         $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/form']['beforeFormCreate'][1489959059]
-            = Bitmotion\MarketingAutomationMautic\Hooks\MauticFormHook::class;
+            = \Bitmotion\MarketingAutomationMautic\Hooks\MauticFormHook::class;
 
         $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/form']['beforeFormDuplicate'][1489959059]
-            = Bitmotion\MarketingAutomationMautic\Hooks\MauticFormHook::class;
+            = \Bitmotion\MarketingAutomationMautic\Hooks\MauticFormHook::class;
 
         $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/form']['beforeFormDelete'][1489959059]
-            = Bitmotion\MarketingAutomationMautic\Hooks\MauticFormHook::class;
+            = \Bitmotion\MarketingAutomationMautic\Hooks\MauticFormHook::class;
 
         $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/form']['beforeFormSave'][1489959059]
-            = Bitmotion\MarketingAutomationMautic\Hooks\MauticFormHook::class;
+            = \Bitmotion\MarketingAutomationMautic\Hooks\MauticFormHook::class;
     }
 
     $slotDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class);
