2941899c70fd2f663d89838b1465d482a21259d2
Florian Wessels <f.wessels@bitmotion.de>
2018-09-03 14:08:18 +0200

    [TASK] Add linebreak to code

--- Classes/Service/MauticTrackingService.php
+++ Classes/Service/MauticTrackingService.php
@@ -37,6 +37,7 @@ class MauticTrackingService implements SingletonInterface
         return '(function(w,d,t,u,n,a,m){w[\'MauticTrackingObject\']=n;'
             . 'w[n]=w[n]||function(){(w[n].q=w[n].q||[]).push(arguments)},a=d.createElement(t),m=d.getElementsByTagName(t)[0];'
             . 'a.async=1;a.src=u;m.parentNode.insertBefore(a,m)})(window,document,\'script\','
-            . GeneralUtility::quoteJSvalue($this->extensionConfiguration['baseUrl'] . '/mtc.js') . ',\'mt\');mt(\'send\', \'pageview\');';
+            . GeneralUtility::quoteJSvalue($this->extensionConfiguration['baseUrl'] . '/mtc.js')
+            . ',\'mt\');mt(\'send\', \'pageview\');';
     }
 }
